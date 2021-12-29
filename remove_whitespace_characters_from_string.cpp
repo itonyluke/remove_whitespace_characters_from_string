3195b47e57b09161deeeea0bd5a4130a5aa00cc8
Anton Pavlyukevich <69690074+itonyluke@users.noreply.github.com>
2021-12-29 17:24:29 +0300

    Update remove_whitespace_characters_from_string.cpp

--- remove_whitespace_characters_from_string.cpp
+++ remove_whitespace_characters_from_string.cpp
@@ -14,7 +14,7 @@
 //along with remove_whitespace_characters_from_string.  If not, see <https://www.gnu.org/licenses/>.
 
 //
-// Created by pavlyukevich.anton@gmail.com on 27.12.21
+// Created by pavlyukevich.anton@gmail.com on 29.12.21
 //
 
 // built and tested on Darwin Kernel Version 18.7.0: Tue Jun 22 19:37:08 PDT 2021; root:xnu-4903.278.70~1/RELEASE_X86_64 x86_64
